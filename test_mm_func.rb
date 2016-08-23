55759311c66d1d8fc722f59c0b0d26214441362d
JTong <eldertong@gmail.com>
2016-08-23 14:42:21 -0400

    removed function from test

--- test_mm_func.rb
+++ test_mm_func.rb
@@ -1,63 +1,7 @@
-# 1.times do
-# 	puts "1" * 100
-# end
-
-# 100.times do
-# 	puts 1
-# end
-
-# 100.times do
-# 	puts * * (number = number + 1)
-# end
-
-
-# number = 1 
-# numbers = number + 1
-
-# 100.times do
-# 	puts number
-
-# end
-
-# print 1.step(100).to_a
-
-# (1..100).each do |face|
-# 	puts face
-# end
-
-
-# x = 1
-# 100.times do
-# 	puts x
-# 	x = x + 1
-# 	if x % 3 and x % 5
-# 		puts "Mined Minds"
-# 	elsif x % 3 == 0
-# 		puts "Mined"
-# 	elsif x % 5 == 0
-# 		puts "Minds"
-# 	else
-# 		puts x
-# 	end
-# end
-
-# 1.upto(100) do |x|
-#       if x % 5 == 0 and x % 3 == 0
-#         puts "Mined Minds"
-#       elsif x % 5 == 0
-#         puts "Mined"
-#       elsif x % 3 == 0
-#         puts "Minds"
-#       else
-#         puts x
-#       end
-#     end
 
 require "minitest/autorun"
 
-def mined_minds(number)
-	1
-end
+
 
 class TestMinedMindsFunction < Minitest::Test 
 	def test_1_equals_1
@@ -69,18 +13,4 @@ class TestMinedMindsFunction < Minitest::Test
 
 
 
-end
-
-number = 1
-100.times do
-	if number % 5 == 0 and number % 3 == 0
-		puts "Mined Minds"
-	elsif number % 3 == 0
-		puts "Mined"
-	elsif number % 5 == 0
-		puts "Minds"
-	else
-		puts number
-	end
-	number = number + 1
 end
