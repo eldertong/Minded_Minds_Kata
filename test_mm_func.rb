76bb954e5143b4d8eb063ce5d0357a1030cf2f04
JTong <eldertong@gmail.com>
2016-08-23 14:36:42 -0400

    First Test 1 returns 1

--- test_mm_func.rb
+++ test_mm_func.rb
@@ -53,6 +53,24 @@
 #       end
 #     end
 
+require "minitest/autorun"
+
+def mined_minds(number)
+	1
+end
+
+class TestMinedMindsFunction < Minitest::Test 
+	def test_1_equals_1
+		assert_equal(1, 1)
+	end
+	def test_1_returns_1
+		assert_equal(1, mined_minds(1))
+	end
+
+
+
+end
+
 number = 1
 100.times do
 	if number % 5 == 0 and number % 3 == 0
